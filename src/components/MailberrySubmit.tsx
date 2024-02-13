fe4d5baa6e4d0b614e570068eb83f862579071bd
Darwin_N19 <darwinccn25@gmail.com>
2024-02-12 23:06:40 -0500

    added submitStyles object that contains the button and wrapper styles

--- src/components/MailberrySubmit.tsx
+++ src/components/MailberrySubmit.tsx
@@ -3,8 +3,10 @@ import { FormContext } from "./MailberryForm";
 
 type MailberrySubmitProps = {
   text: string;
-  buttonWrapperStyles?: React.CSSProperties;
-  buttonStyles?: React.CSSProperties;
+  submitStyles?: {
+    wrapper?: React.CSSProperties;
+    button?: React.CSSProperties;
+  }
 }
 
 const defaultWrapperStyles: React.CSSProperties = {
@@ -21,12 +23,13 @@ const defaultButtonStyles: React.CSSProperties = {
   cursor: 'pointer'
 }
 
-const MailberrySubmit: React.FC<MailberrySubmitProps> = ({ text, buttonStyles = {}, buttonWrapperStyles = {} }) => {
+const MailberrySubmit: React.FC<MailberrySubmitProps> = ({ text, submitStyles = {} }) => {
+  const { button: buttonStyles = {}, wrapper: buttonWrapperStyles  = {} } = submitStyles;
   const { isSubmitting } = useContext(FormContext);
 
   // TODO: Pasar las prps font size, font family, color y bg color desde el front como styles con la prop de buttonStyles
   return (
-    <div  style={{...defaultWrapperStyles, ...buttonWrapperStyles}}>
+    <div style={{...defaultWrapperStyles, ...buttonWrapperStyles}}>
       <button type="submit" disabled={isSubmitting} style={{...defaultButtonStyles, ...buttonStyles}}>{text}</button>
     </div>
   )
